969ff5e92beee591f80aac71910347305b181a02
Lemon <516330797@qq.com>
2021-01-25 22:47:16 +0800

    Money.tsx 之 备注 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -29,7 +29,29 @@ const TagsSection = styled.section`
   }
 `;
 
-const NotesSection = styled.section``;
+const NotesSection = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+
+  > label {
+    display: flex;
+    align-items: center;
+
+    > span {
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+
+    > input {
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
+`;
 
 const CategorySection = styled.section``;
 
@@ -50,7 +72,7 @@ function Money() {
       <NotesSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder="在这里添加备注"/>
         </label>
       </NotesSection>
       <CategorySection>
