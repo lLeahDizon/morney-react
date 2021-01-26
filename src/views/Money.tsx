0efef00e4747e9ef0ca30d13b37032a983fa25ff
Lemon <516330797@qq.com>
2021-01-27 07:10:01 +0800

    Money.tsx 之 数字面板 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -80,7 +80,63 @@ const CategorySection = styled.section`
   }
 `;
 
-const NumberPadSection = styled.section``;
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  > .output {
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+  }
+
+  > .pad {
+    > button {
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 64px;
+      border: none;
+
+      &.ok {
+        float: right;
+        height: 192px;
+      }
+
+      &:nth-child(1) {
+        background: #f2f2f2;
+      }
+
+      &:nth-child(2), &:nth-child(5) {
+        background: #E0E0E0;
+      }
+
+      &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+        background: #D3D3D3;
+      }
+
+      &:nth-child(4), &:nth-child(7), &:nth-child(10), &:nth-child(12) {
+        background: #C1C1C1;
+      }
+
+      &:nth-child(11), &:nth-child(13) {
+        background: #BBBBBB;
+      }
+
+      &:nth-child(8) {
+        background: #9A9A9A;
+      }
+
+      &:nth-child(14) {
+        background: #A9A9A9;
+      }
+    }
+  }
+`;
 
 function Money() {
   return (
@@ -107,8 +163,8 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className='output'>100</div>
+        <div className='pad clearfix'>
           <button>1</button>
           <button>2</button>
           <button>3</button>
@@ -116,11 +172,11 @@ function Money() {
           <button>4</button>
           <button>5</button>
           <button>6</button>
-          <button>清空</button>
+          <button className='ok'>OK</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
+          <button>清空</button>
           <button>0</button>
           <button>.</button>
         </div>
