1fb168201d0abdf6fc451feeaf0d7da326901a43
Lemon <516330797@qq.com>
2021-01-28 17:11:05 +0800

    CategorySection 切换功能搞定

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
 
   > ul {
@@ -26,5 +27,24 @@ const CategorySection = styled.section`
     }
   }
 `;
+
+const CategorySection: React.FC = () => {
+  const categoryMap = {'-': '支出', '+': '收入'};
+  type Keys = keyof typeof categoryMap
+  const [categoryList] = useState<Keys[]>(['-', '+']);
+  const [category, setCategory] = useState('-');// + 表示收入 - 表示支出
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c =>
+          <li className={category === c ? 'selected' : ''}
+              onClick={() => {setCategory(c);}}
+          >{categoryMap[c]}
+          </li>
+        )}
+      </ul>
+    </Wrapper>
+  );
+};
 
 export {CategorySection};
