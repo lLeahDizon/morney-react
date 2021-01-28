d1da94bd2a1ae4f39d55fa211e8e8aec60c77b2d
Lemon <516330797@qq.com>
2021-01-29 07:11:55 +0800

    数字按钮功能基本完成

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -60,7 +60,15 @@ const Wrapper = styled.section`
 `;
 
 const NumberPadSection: React.FC = () => {
-  const [output, setOutput] = useState('0');
+  const [output, _setOutput] = useState('0');
+  const setOutput = (output: string) => {
+    if (output.length > 16) {
+      output = output.slice(0, 16);
+    } else if (output.length === 0) {
+      output = '0';
+    }
+    _setOutput(output);
+  };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if (text === null) { return; }
@@ -75,7 +83,6 @@ const NumberPadSection: React.FC = () => {
       case '7':
       case '8':
       case '9':
-      case '.':
         setOutput(text);
         if (output === '0') {
           setOutput(text);
@@ -83,11 +90,19 @@ const NumberPadSection: React.FC = () => {
           setOutput(output + text);
         }
         break;
+      case '.':
+        if (output.indexOf('.') >= 0) { return; }
+        setOutput(output + '.');
+        break;
       case '清空':
-        console.log('清空');
+        setOutput('');
         break;
       case '删除':
-        console.log('删除');
+        if (output.length === 1) {
+          setOutput('');
+        } else {
+          setOutput(output.slice(0, -1));
+        }
         break;
       case 'OK':
         console.log('OK');
