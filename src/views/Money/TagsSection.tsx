28ad7041ccbe6a8c008e5b70c9d973e3e041c985
Lemon <516330797@qq.com>
2021-01-31 13:28:23 +0800

    创建 createId，让 id 自增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from 'useTags';
+import {createId} from 'lib/createId';
 
 const Wrapper = styled.section`
   background: #FFFFFF;
@@ -48,7 +49,7 @@ const TagsSection: React.FC<Props> = (props) => {
   const onAddTag = () => {
     const tagName = window.prompt('新标签的名称为');
     if (tagName !== null && tagName.trim() !== '') {
-      setTags([...tags, {id: Math.random(), name: tagName}]);
+      setTags([...tags, {id: createId(), name: tagName}]);
     }
   };
   const onToggleTag = (tagId: number) => {
