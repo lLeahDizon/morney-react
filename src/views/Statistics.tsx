593c860188b4a01f32da59ad41ba49ba871e6ff6
Lemon <516330797@qq.com>
2021-02-18 09:35:33 +0800

    消除警告

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -38,7 +38,7 @@ function Statistics() {
   const hash: { [K: string]: RecordItem[] } = {};
   const selectedRecords = records.filter(r => r.category === category);
 
-  selectedRecords.map(r => {
+  selectedRecords.forEach(r => {
     const key = day(r.createdAt).format('YYYY年MM月DD日');
     if (!(key in hash)) {
       hash[key] = [];
